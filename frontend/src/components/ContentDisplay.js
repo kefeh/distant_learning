03cbde792a4f7c389d95a00de512f7bdc5d5b46f
anga4 <anga4brandon500@gmail.com>
2020-05-20 21:34:20 +0100

    Connected all endpoint for list page

--- frontend/src/components/ContentDisplay.js
+++ frontend/src/components/ContentDisplay.js
@@ -1,23 +1,11 @@
 import React, { Component } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import "../stylesheets/ContentDisplay.css";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: "100%",
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-}));
+function DisplayClasses({ classItem }) {
+    return <div className={`my-3 box-shadow texting-styling`}>{classItem.name}</div>;
+}
 
-class AccordionCollapseDisplay extends Component {
+class DisplaySubCategory extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -38,58 +26,30 @@ class AccordionCollapseDisplay extends Component {
             }));
         }
     };
-
     render() {
-        const { child } = this.props;
-        return child.children && child.children.length > 0 ? (
+        const { subCategory } = this.props;
+        return (
             <>
                 <div
                     className={`my-3 hover__cursor__style texting-styling ${
-                        child.id + child.name === this.state.selectedBlock ? null : "box-shadow"
+                        subCategory.id + subCategory.name === this.state.selectedBlock ? "selected__box__style" : "box-shadow"
                     }`}
-                    onClick={() => this.toggleDrop(child)}
+                    onClick={() => this.toggleDrop(subCategory)}
                 >
-                    {/* <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        style={{ backgroundColor: "#e5e5e5" }}
-                        aria-controls="panel1a-content"
-                        id={child.id}
+                    {subCategory.name}
+                </div>
+                {subCategory.classes && subCategory.classes.length > 0 && (
+                    <div
+                        className={`ml-5 ${
+                            subCategory.id + subCategory.name === this.state.selectedBlock ? "d-block" : "d-none"
+                        }`}
                     >
-                        <Typography className={classes.heading}>{child.name}</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <div className="container">
-                            {child.children.map((subChild) => (
-                                <div key={subChild.id} className="row">
-                                    <AccordionCollapseDisplay child={subChild} />
-                                </div>
-                            ))}
-                        </div>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel> */}
-                    {child.name}
-                </div>
-                <div className={`ml-5 ${child.id + child.name === this.state.selectedBlock ? "d-block" : "d-none"}`}>
-                    {child.children.map((subChild) => (
-                        <AccordionCollapseDisplay child={subChild} />
-                    ))}
-                </div>
+                        {subCategory.classes.map((classItem) => (
+                            <DisplayClasses key={classItem.id} classItem={classItem} />
+                        ))}
+                    </div>
+                )}
             </>
-        ) : (
-            <div className={`my-3 box-shadow texting-styling`}>
-                {/* <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        onClick={() => window.alert("done")}
-                        style={{ backgroundColor: "#e5e5e5" }}
-                        aria-controls="panel1a-content"
-                        id={child.id}
-                    >
-                        <Typography>{child.name}</Typography>
-                    </ExpansionPanelSummary>
-                </ExpansionPanel> */}
-                {child.name}
-            </div>
         );
     }
 }
@@ -98,18 +58,25 @@ class ContentDisplay extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row justify-content-center">
-                    {this.props.leaveData && this.props.leaveData.length > 0 ? (
-                        this.props.leaveData.map((item) => (
-                            <div key={item.id} className="col-5">
-                                <h5 className="text-left">{item.name}</h5>
-                                {item.children && item.children.length > 0
-                                    ? item.children.map((child) => <AccordionCollapseDisplay key={child.id} child={child} />)
-                                    : null}
+                <div className="row justify-content-around">
+                    {this.props.categories && this.props.categories.length > 0 ? (
+                        this.props.categories.map((category) => (
+                            <div key={category.id} className="col-3">
+                                <h5 className="text-center">{category.name}</h5>
+                                {category.classes &&
+                                    category.classes.length > 0 &&
+                                    category.classes.map((classItem) => (
+                                        <DisplayClasses key={classItem.id} classItem={classItem} />
+                                    ))}
+                                {category.sub_categories &&
+                                    category.sub_categories.length > 0 &&
+                                    category.sub_categories.map((subCategory) => (
+                                        <DisplaySubCategory key={subCategory.id} subCategory={subCategory} />
+                                    ))}
                             </div>
                         ))
                     ) : (
-                        <div className="alert alert-danger" role="alert">
+                        <div className="col alert alert-danger" role="alert">
                             No Data Available
                         </div>
                     )}
