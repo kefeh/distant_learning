e13d876484e0de29bc42bea3270da2808eb10442
kefeh <kefehcollins@gmail.com>
2020-05-27 21:10:51 +0100

    Updating the headers and adding The republic of Cameroon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,13 +11,16 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <div className="national-colors"></div>
+        <div className="national-colors">
+          <span>République du Cameroun</span>
+          <span>Republic of Cameroon</span>
+        </div>
         <div className='logo'>
           <span className='logo__ministry'>MINISTRY OF SECONDARY EDUCATION</span>
           <span className='logo__republic'>MINISTERE DES ENSEIGNEMENTS SECONDAIRES</span>
         </div>
         <div className="App-header">
-          <h5 onClick={() => {this.navTo('')}}>Distant Education</h5>
+          <h5 onClick={() => {this.navTo('')}}>Distance Education</h5>
           <h5 onClick={() => {this.navTo('')}}>Télé-Enseignement</h5>
           {/* <h5 onClick={() => {this.navTo('/add')}}>Admin Dashboard</h5>
           <h5 onClick={() => {this.navTo('/play')}}>Videos</h5> */}
